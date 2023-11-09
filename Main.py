e6c5d76456006416126b06eda978d98bbda30984
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-11-09 10:37:53 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,56 +3,34 @@ import plotly.express as px
 import pandas as pd
 from measures import *
 #import seaborn as sns
-###from scipy.stats import ttest_ind
-
-
-
-
-    
-
-   
+from scipy.stats import ttest_ind
 
 selected_measurement = st.selectbox("Choose a measurement", measurements)
 data = pd.read_csv("Reference_Prototype.csv")
 phase_id = data['Phase'].unique()
 unit_id = data['Unit'].unique()
 
-#selected_phase = st.multiselect('Select tower(s)', options = phase_id, default=phase_id)
-#selected_unit = st.multiselect('Select tower(s)', options = unit_id, default=unit_id)
-
+selected_phase = st.multiselect('Select Phase(s)', options = phase_id, default=phase_id)
 
 average = data[selected_measurement].mean()
 stdev = data[selected_measurement].std()
 
 Reference = data[data["Unit"] == "Reference"]
 Prototype = data[data["Unit"] == "Prototype"]
-##t_test = ttest_ind(Reference[selected_measurement], Prototype[selected_measurement])
 
-##st.write(t_test)
+Reference = Reference[Reference['Phase'].isin(selected_phase)]
+Prototype = Prototype[Prototype['Phase'].isin(selected_phase)]
+data = data[data['Phase'].isin(selected_phase)]
 
-UCL = average + (3*stdev)
-LCL = average - (3*stdev)
-
-control = data[(data[selected_measurement] >= LCL) & (data[selected_measurement] <= UCL)]
-outside = data[(data[selected_measurement] < LCL) | (data[selected_measurement] > UCL)]
-
-dfg = data.groupby(['Unit'])[selected_measurement].mean()
-
-stdev = data.groupby(['Unit'])[selected_measurement].std()
-
-#bar = dfg.plot(kind='bar', title=f'Prototype vs Reference Mean of {selected_measurement}', ylabel=f'Mean of {selected_measurement}',
-         #xlabel='Unit', figsize=(6, 5))
+t_test = ttest_ind(Reference[selected_measurement], Prototype[selected_measurement], equal_var=False)
 
 
-fig = px.box(data, x='Unit', y=selected_measurement, title=f"{selected_measurement} Box Plot", color='Unit',)
+pvalue = t_test[1]
+st.write(pvalue)
+if pvalue >= 0.05:
+    st.write(f"The P value is {pvalue} and greater than 0.05 and the Prototype and References are likely the same")
+else:
+    st.write(f"The P value is {pvalue} and less than 0.05 and the Prototype and References are likely different")
 
-
-#st.plotly_chart(bar, use_container_width=True)
-
-
-bar = px.bar(dfg, title=f"Mean of {selected_measurement}, Prototype vs Reference")
-bar2 = px.bar(stdev, title=f"Standard Deviation of {selected_measurement}, Prototype vs Reference")
-st.plotly_chart(bar, use_container_width=True)
-st.plotly_chart(bar2, use_container_width=True)
-
+fig = px.box(data, x='Unit', y=selected_measurement, title=f"{selected_measurement} Box Plot of the Selected Phase(s)", color='Unit',)
 st.plotly_chart(fig, use_container_width=True)
